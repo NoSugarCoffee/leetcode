cbcc8ed5b367049762abafcb404323bad479293d
liangliang.dai <liangliang.dai@leyantech.com>
2021-02-09 18:41:04 +0800

    add assignCookies and candy solution

---
+++ src/chapter_two/AssignCookiesTest.java
@@ -0,0 +1,14 @@
+package chapter_two;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AssignCookiesTest {
+  @Test
+  public  void test() {
+    AssignCookies.Solution solution = new AssignCookies().new Solution();
+    int []appetites =new int[]{1,2};
+    int []cookies = new int[]{1, 2, 3};
+    Assert.assertEquals(2, solution.findContentChildren(appetites,cookies));
+  }
+}
---
+++ src/chapter_two/CandyTest.java
@@ -0,0 +1,16 @@
+package chapter_two;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+  public class CandyTest {
+  @Test
+  public void test() {
+    //[[0,1,2,5,3,2,7]
+    int[] ratings = new int[]{0,1,2,5,3,2,7};
+    Assert.assertEquals(15, new Candy().new Solution().candy(ratings));
+
+    ratings = new int[]{1, 2, 2};
+    Assert.assertEquals(4, new Candy().new Solution().candy(ratings));
+  }
+}
