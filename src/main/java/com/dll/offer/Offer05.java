6baf6f1de3805703b1a34ee3097317589d7bb7fe
liangliang.dai <liangliang.dai@leyantech.com>
2021-06-30 10:28:10 +0800

    feat(github-action): add maven test

---
+++ src/main/java/com/dll/offer/Offer05.java
@@ -0,0 +1,23 @@
+package com.dll.offer;
+
+import java.util.stream.Stream;
+
+public class Offer05 {
+  class Solution {
+    public String replaceSpace(String s) {
+      if (s == null) {
+        return "";
+      }
+      StringBuilder sb = new StringBuilder();
+      Stream.of(s.split("")).forEach(ss -> {
+        System.out.println(ss);
+        if (" ".equals(ss)) {
+          sb.append("%20");
+        } else {
+          sb.append(ss);
+        }
+      });
+      return sb.toString();
+    }
+  }
+}
---
+++ src/test/java/com/dll/offer/Offer05Test.java
@@ -0,0 +1,15 @@
+package com.dll.offer;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Offer05Test {
+
+  @Test
+  public void test() {
+    Assert.assertEquals("We%20are%20happy.",
+        new Offer05().new Solution().replaceSpace("We are happy."));
+    Assert.assertEquals("",
+        new Offer05().new Solution().replaceSpace(null));
+  }
+}
