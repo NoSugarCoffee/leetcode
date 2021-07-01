ca0323cb176182591456e4ede72aa2e9a86b985d
liangliang.dai <liangliang.dai@leyantech.com>
2021-07-01 20:47:15 +0800

    feat(offer): solve problem 09

---
+++ src/main/java/com/dll/offer/Offer09.java
@@ -0,0 +1,36 @@
+package com.dll.offer;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+public class Offer09 {
+  class CQueue {
+    Deque<Integer> inQueueStack = new LinkedList<>();
+    Deque<Integer> deQueueStack = new LinkedList<>();
+
+    public CQueue() {
+    }
+
+    public void appendTail(int value) {
+      inQueueStack.push(value);
+    }
+
+    private int popWrapper() {
+      try {
+        return deQueueStack.pop();
+      } catch (NoSuchElementException e) {
+        return -1;
+      }
+    }
+
+    public int deleteHead() {
+      if (deQueueStack.isEmpty()) {
+        while (!inQueueStack.isEmpty()) {
+          deQueueStack.push(inQueueStack.pop());
+        }
+      }
+      return popWrapper();
+    }
+  }
+}
