86ecd247f99c68236fde97ffa4bffc1dd1c35b36
诗序 <1353025854@qq.com>
2021-07-13 00:04:53 +0800

    feat(multithreads): solve problem 1115 (#24)

---
+++ src/main/java/com/dll/multithreads/PrintFoobarAlternately.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	chFor, chBar := make(chan int, 1), make(chan int, 1)
+	wg.Add(2)
+	chFor <- 1
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 10; i++ {
+			if _, ok := <-chFor; ok {
+				fmt.Println("foo")
+				chBar <- 1
+			}
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 10; i++ {
+			if _, ok := <-chBar; ok {
+				fmt.Println("bar")
+				chFor <- 1
+			}
+		}
+	}()
+	wg.Wait()
+	close(chFor)
+	close(chBar)
+}
