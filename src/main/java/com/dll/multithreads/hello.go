e4fb416526b83fc69b67c27ab17f9291dcd60edc
NoSugarCoffee <1353025854@qq.com>
2021-07-12 23:40:26 +0800

    update

--- src/main/java/com/dll/multithreads/hello.go
+++ src/main/java/com/dll/multithreads/hello.go
@@ -1,18 +1,19 @@
 package main
+
 import "fmt"
 import "sync"
+
 func main() {
     var wg sync.WaitGroup
-    ch_for, ch_bar := make(chan int, 1), make(chan int, 1)
+    chFor, chBar := make(chan int, 1), make(chan int, 1)
     wg.Add(2)
-    ch_for <- 1
+    chFor <- 1
     go func() {
         defer wg.Done()
         for i :=0; i < 10; i++ {
-            select {
-                case <- ch_for:
-                    fmt.Println("foo")
-                    ch_bar <- 1
+            if _, ok := <- chFor; ok {
+                fmt.Println("foo")
+                chBar <- 1
             }
         }
     }()
@@ -20,10 +21,9 @@ func main() {
     go func() {
         defer wg.Done()
         for i := 0; i < 10; i++ {
-            select {
-                case <- ch_bar:
-                    fmt.Println("bar")
-                    ch_for <- 1
+            if _, ok := <- chBar; ok {
+                fmt.Println("bar")
+                chFor <- 1
             }
         }
     }()
