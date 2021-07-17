34278cc14dd81cda1da1ccc9c8cd83006b5d6d23
诗序 <1353025854@qq.com>
2021-07-17 14:38:42 +0800

    feat(hashtable): solve problem 242 (#31)
    * feat(hashtable): solve problem 242
    
    * update
    
    * update
    
    * update

---
+++ src/main/java/com/dll/hashtable/ValidAnagram.java
@@ -0,0 +1,24 @@
+package com.dll.hashtable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidAnagram {
+    class Solution {
+        private Map<Character, Integer> toCharMap(String s) {
+            Map<Character, Integer>  map = new HashMap<>();
+            char[] chars = s.toCharArray();
+            for (char c: chars) {
+                int val = map.getOrDefault(c, 0);
+                map.put(c, val + 1);
+            }
+            return map;
+        }
+        public boolean isAnagram(String s, String t) {
+            if (s.length() != t.length()) {
+                return false;
+            }
+            return this.toCharMap(s).equals(this.toCharMap(t));
+        }
+    }
+}
