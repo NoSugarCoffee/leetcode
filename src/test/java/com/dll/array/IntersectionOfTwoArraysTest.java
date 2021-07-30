6da0aafa8d85a73685262ca1a16e604296dccce3
诗序 <1353025854@qq.com>
2021-07-30 23:27:35 +0800

    feat(array): solve problem 349 (#55)

---
+++ src/test/java/com/dll/array/IntersectionOfTwoArraysTest.java
@@ -0,0 +1,14 @@
+package com.dll.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IntersectionOfTwoArraysTest {
+    @Test
+    public void test() {
+        Assert.assertArrayEquals(new int[]{2}, new IntersectionOfTwoArrays()
+                .new Solution().intersection(new int[]{1, 2, 2, 1}, new int[]{2, 2}));
+
+    }
+}
+
