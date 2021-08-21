0679b26f38485f686e1f819938ca1cf487375cc2
NoSugarCoffee <1353025854@qq.com>
2021-08-21 23:09:36 +0800

    feat(string): solve problem

---
+++ src/test/java/com/dll/string/StringCompressionTest.java
@@ -0,0 +1,25 @@
+package com.dll.string;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StringCompressionTest {
+    @Test
+    public void testSplitNum() {
+        Assert.assertArrayEquals(new int[]{3}, new StringCompression().new Solution().splitNum(3));
+        Assert.assertArrayEquals(new int[]{3, 1}, new StringCompression().new Solution().splitNum(31));
+
+    }
+
+    @Test
+    public void testCompress() {
+        Assert.assertEquals(6, new StringCompression().new Solution()
+                .compress(new char[]{'a','a','b','b','c','c','c'}));
+
+        Assert.assertEquals(4, new StringCompression().new Solution()
+                .compress(new char[]{'a','b','b','b','b','b','b','b','b','b','b','b','b'}));
+    }
+
+}
