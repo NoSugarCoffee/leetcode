c9779d4118cf54f37d99b0f5969a4fbbff5c19a5
NoSugarCoffee <1353025854@qq.com>
2023-08-12 13:21:08 +0800

    solution(23): MergeKSortedLists

---
+++ src/main/java/com/dll/linkedList/MergeKSortedLists23.java
@@ -0,0 +1,23 @@
+package com.dll.linkedList;
+
+class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode() {}
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+public class MergeKSortedLists_23 {
+  class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {}
+  }
+}
