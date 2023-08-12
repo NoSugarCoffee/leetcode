0011fd9e7ab0918d91d6e3927d8ec35894fed48d
NoSugarCoffee <1353025854@qq.com>
2023-08-12 13:22:41 +0800

    solution(23): MergeKSortedLists

--- src/main/java/com/dll/linkedList/MergeKSortedLists23.java
+++ src/main/java/com/dll/linkedList/MergeKSortedLists23.java
@@ -16,8 +16,38 @@ class ListNode {
   }
 }
 
-public class MergeKSortedLists_23 {
+public class MergeKSortedLists23 {
   class Solution {
-    public ListNode mergeKLists(ListNode[] lists) {}
+    ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+      ListNode head = new ListNode(-1, null);
+      ListNode last = head;
+      while (l1 != null && l2 != null) {
+        last.next = new ListNode(Math.min(l1.val, l2.val), null);
+        last = last.next;
+        if (l1.val <= l2.val) {
+          l1 = l1.next;
+        } else {
+          l2 = l2.next;
+        }
+      }
+      if (l1 == null) {
+        last.next = l2;
+      } else {
+        last.next = l1;
+      }
+      return removeHeadIfExist(head);
+    }
+
+    private ListNode removeHeadIfExist(ListNode head) {
+      return (head.val == -1) ? head.next : head;
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+      ListNode previousMerged = null;
+      for (ListNode list : lists) {
+        previousMerged = mergeTwoLists(previousMerged, list);
+      }
+      return previousMerged;
+    }
   }
 }
