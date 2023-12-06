683ca0529f4a51f9fcf80d12422723ebf76680bf
liangliang.dai <liangliang.dai@trip.com>
2023-12-06 21:07:41 +0800

    aoc: solve part two of day2

--- golang/aoc2023/cube_conundrum.go
+++ golang/aoc2023/cube_conundrum.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-func PartOne(input string) int {
+func CubeCountingPartOne(input string) int {
 	var re = regexp.MustCompile(`(?mi)(\d+) (\w+)[,;]?`)
 	lines := strings.Split(input, "\n")
 	result := 0
@@ -27,6 +27,29 @@ func PartOne(input string) int {
 		if m["red"] <= 12 && m["green"] <= 13 && m["blue"] <= 14 {
 			result += i + 1
 		}
+	}
+	return result
+}
+
+func CubeCountingPartTwo(input string) int {
+	var re = regexp.MustCompile(`(?mi)(\d+) (\w+)[,;]?`)
+	lines := strings.Split(input, "\n")
+	result := 0
+	for _, line := range lines {
+		m := map[string]int{
+			"red":   1,
+			"green": 1,
+			"blue":  1,
+		}
+		match := re.FindAllStringSubmatch(line, -1)
+		for _, group := range match {
+			count, _ := strconv.Atoi(group[1])
+			color := group[2]
+			if exist, ok := m[color]; ok && exist < count {
+				m[color] = count
+			}
+		}
+		result += m["red"] * m["green"] * m["blue"]
 	}
 	return result
 }
